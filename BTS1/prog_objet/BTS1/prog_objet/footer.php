970bc4e2ebcb32b16296b32cb8d37221acd77341
Arthur Bayard <ruhtramanb@gmail.com>
2025-12-15 16:52:56 +0100

    Add files via upload

---
+++ BTS1/prog_objet/BTS1/prog_objet/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="footer-content">
+        <p>&copy; 2025 Prog Objet. Tous droits réservés.</p>
+        <p>Contact: info@unmail.com</p>
+        <p>Site réalisé avec PHP et MySQL</p>
+    </div>
+</footer>
---
+++ BTS1/prog_objet/BTS1/prog_objet/header.php
@@ -0,0 +1,4 @@
+<header>
+    <a href='/prog_objet/index.php'><img src="\prog_objet\ressources\home_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="accueil" name='accueil_icon'></a>
+    <a href='/prog_objet/ajout_actualite/index.php'><img src="\prog_objet\ressources\add_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="ajout actualité" name='ajout_icon'></a>
+</header>
